3607552bb32c9ea2a5e47c4011ebba5590481901
Sébastien Dabet <sebastiendabet@gmail.com>
2011-12-09 18:37:41 +0100

    Copy of the master branch (no minification)

--- script.js
+++ script.js
@@ -1,1 +1,431 @@
-Array.prototype.remove=function(a,b){var c=this.slice((b||a)+1||this.length);this.length=a<0?this.length+a:a;return this.push.apply(this,c)};var fps=30;var container=document.getElementById("container");var board=document.getElementById("board");var wallNumber=15;var gumNumber=15;var boardWidth=800;var boardHeight=580;var level=1;var highScore=99.99;var unitHeight=30;var gums=[];var walls=[];var timer;var ballImg=new Image;var wallImg=new Image;var gumImg=new Image;ballImg.style.position="absolute";ballImg.className="ball";gumImg.style.position="absolute";gumImg.className="gum";wallImg.style.position="absolute";wallImg.className="wall";var setLevel=function(a){level=a;boule.speed=500+a*50;document.getElementById("level").innerHTML=level+1};var start=function(){container.className="";watch.start();if(!timer){timer=setInterval(function(){boule.animate();checkCollisions()},1e3/fps)}};var setDisplay=function(a,b){var c=document.getElementsByClassName(a);for(i=0;i<c.length;i++){c[i].style.display=b}};var showDialog=function(a){setDisplay("dialog","none");setDisplay("modal","block");document.getElementById(a).style.display="inline-block"};var win=function(){stop();var a=Math.round(watch.elapsed/10)/100;highScore=Math.min(highScore,a);document.getElementById("score").innerHTML=a;document.getElementById("high_score").innerHTML=highScore;showDialog("win");setLevel(level+1)};var lose=function(){stop();container.className="crash";setTimeout(function(){showDialog("lose")},1500);setLevel(0)};var stop=function(){watch.stop();if(timer){clearInterval(timer);timer=null}};var watch={start_time:0,elapsed:0,start:function(){this.start_time=(new Date).getTime()},stop:function(){this.elapsed=(new Date).getTime()-this.start_time}};var boule={x:0,y:0,w:unitHeight,h:unitHeight,speed:0,draw:function(){var a=this.dom.style;a.top=this.y+"px";a.left=this.x+"px"},animate:function(){this.x+=parseInt(this.speed/fps);if(this.x<=0){this.speed=-this.speed;this.x=0}else if(this.x>=boardWidth-this.w){this.speed=-this.speed;this.x=boardWidth-this.w}this.draw()},up:function(){if(this.y>=this.h){this.y=this.y-this.h}},down:function(){if(this.y<=boardHeight-2*this.h){this.y=this.y+this.h}}};var checkCollisions=function(){if(findCollision(boule.x,boule.y,boule.w,boule.h,walls)!==null){lose();return}var a=findCollision(boule.x,boule.y,boule.w,boule.h,gums);if(a!==null){var b=gums[a];b.dom.style.width="0";b.dom.style.marginLeft="15px";b.dom.style.marginTop="15px";gums.remove(a,a);if(gums.length==0){win()}boule.speed=-boule.speed}};var findCollision=function(a,b,c,d,e){for(var f=0;f<e.length;f++){var g=e[f];var h=a+c>g.x&&a<g.x+g.w;var i=b+d>g.y&&b<g.y+g.h;if(h&&i){return f}}return null};var ready=function(){container.className="ready"};var drawWall=function(a){var b=wallImg.cloneNode(true);b.style.left=walls[a].x+"px";b.style.top=walls[a].y+"px";board.appendChild(b);walls[a].dom=b;if(a<walls.length-1){setTimeout(function(){drawWall(a+1)},50)}else{setTimeout(function(){drawGum(0)},50)}};var drawGum=function(a){var b=gumImg.cloneNode(true);b.style.left=gums[a].x+"px";b.style.top=gums[a].y+"px";board.appendChild(b);gums[a].dom=b;if(a<gums.length-1){setTimeout(function(){drawGum(a+1)},50)}else{ready()}};var startDrawing=function(){drawWall(0)};var randomX=function(){return Math.floor(Math.random()*(boardWidth-unitHeight))};var randomY=function(){return Math.floor(Math.random()*((boardHeight-unitHeight)/unitHeight))*unitHeight};var reset=function(){container.className="";board.innerHTML="";var a=2*unitHeight;boule.x=randomX();boule.y=0;boule.targetY=boule.y;boule.dom=ballImg.cloneNode(true);board.appendChild(boule.dom);boule.draw();gums=[];for(var b=0;b<gumNumber;b++){var c;do{c={x:randomX(),y:randomY(),w:unitHeight,h:unitHeight}}while(findCollision(c.x-a,c.y-a,c.w+2*a,c.h+2*a,gums)!==null);gums[b]=c}walls=[];for(var b=0;b<wallNumber;b++){var d;do{d={x:randomX(),y:randomY(),w:unitHeight,h:unitHeight}}while(findCollision(d.x-a,d.y-a,d.w+2*a,d.h+2*a,gums)!==null||findCollision(d.x-a,d.y-a,d.w+2*a,d.h+2*a,walls)!==null||d.y<=boule.y+boule.h&&d.y+d.h>=boule.y);walls[b]=d}setTimeout(startDrawing,1e3)};var loadImages=function(a){var b=0;var c=function(){b++;if(b==3){a()}};ballImg.addEventListener("load",c,false);wallImg.addEventListener("load",c,false);gumImg.addEventListener("load",c,false);ballImg.src="ball.png";wallImg.src="wall.png";gumImg.src="smiley.png"};var init=function(){setLevel(0);document.addEventListener("keydown",function(a){if(a.keyCode==38){boule.up();return false}if(a.keyCode==40){boule.down();return false}if(a.keyCode==32&&container.className=="ready"){start();return false}},false);var a=document.getElementsByClassName("try_again");for(i=0;i<a.length;i++){a[i].addEventListener("click",function(){setDisplay("modal","none");reset();return false},false)}loadImages(reset)};init()
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+	this.length = from < 0 ? this.length + from : from;
+	return this.push.apply(this, rest);
+};
+
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       || 
+            window.webkitRequestAnimationFrame || 
+            window.mozRequestAnimationFrame    || 
+            window.oRequestAnimationFrame      || 
+            window.msRequestAnimationFrame     || 
+            function(/* function */ callback, /* DOMElement */ element){
+              window.setTimeout(callback, 1000 / 60);
+            };
+  })();
+
+
+//(function(){
+	// The container dom element. It can have the following classes:
+	//  - ready: game is initialized correctly, start message is displayed, waiting for spacebar pressed
+	//  - crash: when collision with wall (red background)
+	var container = document.getElementById("container");
+
+	var board = document.getElementById("board");
+
+	var initSpeed = 500; // pixel per second
+	var wallNumber = 15;
+	var gumNumber = 15;
+	var boardWidth = 800;
+	var boardHeight = 580;
+	var level = 1;
+	var highScore = 99.99;
+
+	/* Base height unit */
+	var unitHeight = 30;
+
+	var gums = [];
+	var walls = [];
+	var timer; // game animation timer
+
+	/*
+	 * The 3 images used in the game (they could also be loaded as dom elements). Walls and gums are cloned at game initialization.
+	 */
+	var ballImg = new Image();
+	var wallImg = new Image();
+	var gumImg = new Image();
+	ballImg.style.position = "absolute";
+	ballImg.className = "ball";
+	gumImg.style.position = "absolute";
+	gumImg.className = "gum";
+	wallImg.style.position = "absolute";
+	wallImg.className = "wall";
+
+	/*
+	 * Set level value and update the ball speed
+	 */
+	var setLevel = function(l) {
+		level = l;
+		boule.speed = initSpeed + l*50;
+		document.getElementById("level").innerHTML = level+1;
+	}
+
+	var stopAnimation = false; // flag to stop animation
+	
+	var lastUpdateTime;
+	
+	/*
+	 * Start game
+	 */
+	var start = function() { 
+		container.className = "";
+
+		// Start stopwatch
+		watch.start();
+		
+		lastUpdateTime = new Date().getTime();
+		stopAnimation = false;
+		loop(lastUpdateTime);
+	};
+	
+	var loop = function(time) {
+		time = time || new Date().getTime();
+		if(!stopAnimation) {
+			requestAnimFrame(loop);
+		}
+		boule.animate(time-lastUpdateTime);
+		checkCollisions();
+		lastUpdateTime = time;
+	}
+
+	/*
+	 * Change the display css property for elements matching a class name
+	 */
+	var setDisplay = function(className, display) {
+		var elts = document.getElementsByClassName(className);
+		for(i = 0; i < elts.length; i++) {
+			elts[i].style.display = display;
+		}
+	};
+
+	/*
+	 * Show the dialog box with the given ID
+	 */
+	var showDialog = function(dialog) {
+		setDisplay("dialog", "none");
+		setDisplay("modal", "block");
+		document.getElementById(dialog).style.display = "inline-block";
+	};
+
+	/*
+	 * Player wins
+	 */
+	var win = function() {
+		// Stop game
+		stop();
+		// Truncate elapsed time (2 digits)
+		var score = Math.round(watch.elapsed / 10) / 100;
+		// Update high score
+		highScore = Math.min(highScore, score);
+		// Update scores displayed in dom
+		document.getElementById("score").innerHTML = score;
+		document.getElementById("high_score").innerHTML = highScore;
+		// Show victory dialog
+		showDialog("win");
+		// Increment level
+		setLevel(level+1);
+	};
+
+	/*
+	 * Player loses
+	 */
+	var lose = function() {
+		// Stop game
+		stop();
+		// Crash animation
+		container.className = "crash";
+		// Display 'game over' dialog after a while
+		setTimeout(function() { showDialog("lose"); }, 1500);
+		// Reset level
+		setLevel(0);
+	};
+
+	/*
+	 * Stop the game
+	 */
+	var stop = function() {
+		// Stop the watch
+		watch.stop();
+		// Stop animation timer
+		stopAnimation = true;
+	};
+
+	/*
+	 * Stop watch implementation
+	 */
+	var watch = {
+		start_time: 0,
+		elapsed: 0,
+
+		start: function() {
+			this.start_time = new Date().getTime();
+		},
+		stop: function() {
+			this.elapsed = new Date().getTime() - this.start_time;
+		}
+	};
+
+	/* 
+	 * Ball implementation 
+	 */
+	var boule = {
+		x: 0,
+		y: 0,
+		w: unitHeight,
+		h: unitHeight,
+		speed: 0, // pixels per second
+		draw: function() {
+			var style = this.dom.style;
+			style.top = this.y + "px";
+			style.left = this.x + "px";
+		},
+		/*
+		 * Update the position and direction of the ball based on its current speed and the delay since last update
+		 */
+		animate: function(delay) {
+			var delta = parseInt(this.speed * delay / 1000);
+			this.x += delta;
+			if(this.x <= 0) {
+				this.speed = -this.speed;
+				this.x = 0;
+			}
+			else if(this.x >= boardWidth-this.w) {
+				this.speed = -this.speed;
+				this.x = boardWidth-this.w;
+			}
+			this.draw();
+		},
+		/*
+		 * Ball goes up
+		 */
+		up: function() {
+			if(this.y >= this.h) {
+				this.y = this.y - this.h;
+			}
+		},
+		/*
+		 * Ball goes down
+		 */
+		down: function() {
+			if(this.y <= boardHeight - 2*this.h) {
+				this.y = this.y + this.h;
+			}
+		}
+	};
+
+	/*
+	 * Check if there is any collision between the ball and anything (wall or gum)
+	 */
+	var checkCollisions = function() {
+		// Wall collision
+		if(findCollision(boule.x, boule.y, boule.w, boule.h, walls) !== null) {
+			lose();
+			return;
+		}
+
+		// Gum collision
+		var index = findCollision(boule.x, boule.y, boule.w, boule.h, gums);
+		if(index !== null) {
+			var gum = gums[index];
+			gum.dom.style.width = "0";
+			gum.dom.style.marginLeft = "15px";
+			gum.dom.style.marginTop = "15px";
+			//gum.dom.parentNode.removeChild(gum.dom);
+			gums.remove(index, index);
+			if(gums.length == 0) {
+				win();
+			}
+			boule.speed = -boule.speed;
+		}
+	};
+
+	/*
+	 * Check if there is a collision between an area and a set of items
+	 */
+	var findCollision = function(x, y, w, h, items) {
+		for(var i = 0; i < items.length; i++) {
+			var item = items[i];
+			var collisionX = x + w > item.x && x < item.x + item.w;
+			var collisionY = y + h > item.y && y < item.y + item.h;
+			if(collisionX && collisionY) {
+				return i;
+			}
+		}
+		return null;
+	};
+
+	/*
+	 * Called when game is ready to start
+	 */
+	var ready = function() {
+		container.className = "ready";
+	};
+
+	/*
+	 * Display walls one after another
+	 */
+	var drawWall = function(i) {
+		var wallEl = wallImg.cloneNode(true);
+		wallEl.style.left = walls[i].x + "px";
+		wallEl.style.top = walls[i].y + "px";
+		board.appendChild(wallEl);
+		walls[i].dom = wallEl;
+		if(i < walls.length - 1) {
+			setTimeout(function() { drawWall(i+1); }, 50);
+		}
+		else {
+			setTimeout(function() { drawGum(0); }, 50);
+		}
+	}
+
+	/*
+	 * Display gums one after another
+	 */
+	var drawGum = function(i) {
+		var gumEl = gumImg.cloneNode(true);
+		gumEl.style.left = gums[i].x + "px";
+		gumEl.style.top = gums[i].y + "px";
+		board.appendChild(gumEl);
+		gums[i].dom = gumEl;
+		if(i < gums.length - 1) {
+			setTimeout(function() { drawGum(i+1); }, 50);
+		}
+		else {
+			ready();
+		}
+	}
+
+	/*
+	 * Start the process of drawing the game items one after another
+	 */
+	var startDrawing = function() {
+		drawWall(0);
+	};
+
+	var randomX = function() {
+		return Math.floor(Math.random()*(boardWidth-unitHeight));
+	}
+	var randomY = function() {
+		return Math.floor(Math.random()*((boardHeight-unitHeight)/unitHeight)) * unitHeight;
+	}
+
+	/*
+	 * Called before starting a new game
+	 */
+	var reset = function() {
+		container.className = "";
+
+		// Clear board
+		board.innerHTML = "";
+
+		var margin = 2 * unitHeight;
+
+		/* Init ball position */
+		boule.x = randomX();
+		boule.y = 0; // on the first line
+		boule.targetY = boule.y;
+		boule.dom = ballImg.cloneNode(true);
+		board.appendChild(boule.dom);
+		boule.draw();
+
+		/* Init gums */
+		gums = [];
+		for(var i=0; i < gumNumber; i++) {
+			var gum;
+			do {
+				gum = {
+					x: randomX(),
+					y: randomY(),
+					w: unitHeight,
+					h: unitHeight
+				};
+			} while(findCollision(gum.x - margin, gum.y - margin, gum.w + 2*margin, gum.h + 2*margin, gums) !== null);
+			gums[i] = gum;
+		}
+
+		/* Init walls */
+		walls = [];
+		for(var i=0; i < wallNumber; i++) {
+			var wall;
+			do {
+				wall = {
+					x: randomX(),
+					y: randomY(),
+					w: unitHeight,
+					h: unitHeight
+				};
+			} while(
+				findCollision(wall.x - margin, wall.y - margin, wall.w + 2*margin, wall.h + 2*margin, gums) !== null
+				|| findCollision(wall.x - margin, wall.y - margin, wall.w + 2*margin, wall.h + 2*margin, walls) !== null
+				|| (wall.y <= boule.y+boule.h && wall.y+wall.h >= boule.y) // no wall on the initial ball row
+			);
+			walls[i] = wall;
+		}
+
+		setTimeout(startDrawing, 1000);
+	};
+
+	/*
+	 * Start loading all the images and execute the provided callback function when they're all loaded
+	 */
+	var loadImages = function(callback) {
+		var nbLoaded = 0;
+		var loadCallback = function() {
+			nbLoaded++;
+			if(nbLoaded == 3) {
+				callback();
+			}
+		};
+		ballImg.addEventListener("load", loadCallback, false);
+		wallImg.addEventListener("load", loadCallback, false);
+		gumImg.addEventListener("load", loadCallback, false);
+		ballImg.src = "ball.png";
+		wallImg.src = "wall.png";
+		gumImg.src = "smiley.png";
+	};
+
+	/*
+	 * Called once to initialize key/dom listeners and load images
+	 */
+	var init = function() {
+		setLevel(0);
+
+		/* Controls */
+		document.addEventListener("keydown", function(e){
+			// Key up
+			if (e.keyCode == 38) {
+				boule.up();
+				e.preventDefault();
+				return false;
+			}
+			// Key down
+			if (e.keyCode == 40) {
+				boule.down();
+				e.preventDefault();
+				return false;
+			}
+			// Spacebar down
+			if (e.keyCode == 32 && container.className == "ready") {
+				start();
+				e.preventDefault();
+				return false;
+			}
+		}, false);
+
+		var tryAgainButtons = document.getElementsByClassName("try_again");
+		for(i = 0; i < tryAgainButtons.length; i++) {
+			tryAgainButtons[i].addEventListener("click", function() {
+				setDisplay("modal", "none");
+				reset();
+				return false;
+			}, false);
+		}
+
+		// Load images
+		loadImages(reset);
+	};
+
+	// Auto-initialize game when page loads
+	init();
+//})();
