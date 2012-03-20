177efd1e72a717ec61d02d197d7f3e46511c17f7
Sébastien Dabet <sebastiendabet@gmail.com>
2012-03-20 12:23:13 +0000

    Use the classList API for class manipulation

--- editor.js
+++ editor.js
@@ -5,19 +5,6 @@ if(location.hash) {
 else {
 
 // Util
-
-function hasClass(ele,cls) {
-    return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
-}
-function addClass(ele,cls) {
-	if (!this.hasClass(ele,cls)) ele.className += " "+cls;
-}
-function removeClass(ele,cls) {
-	if (hasClass(ele,cls)) {
-		var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
-		ele.className=ele.className.replace(reg,' ');
-	}
-}
 function handleFileSelect(evt, callback) {
     var files = evt.target.files; // FileList object
     
@@ -67,10 +54,10 @@ function absPath(url){
 
 function select() {
     for(var i=0; i<selectables.length; i++) {
-        removeClass(selectables[i], "down");
+        selectables[i].classList.remove("down");
     }
     selection = this.getAttribute("type");
-    addClass(this, "down");
+    this.classList.add("down");
     
     switch(selection) {
         case "wall":
